9315f4ca6f847d2aebc9a815c8562461876b2e08
Sebas Arango <sebastianarango201316@hotmail.com>
2024-03-11 00:33:07 +0100

    Added HelperConfig and Deployment Script

--- foundry-defi-stablecoin-f23/script/DeployDSC.s.sol
+++ foundry-defi-stablecoin-f23/script/DeployDSC.s.sol
@@ -2,9 +2,28 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
-  function run() external {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
 
-  }
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, btc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+
+        return (dsc, engine);
+    }
 }
