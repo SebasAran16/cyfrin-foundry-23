7a9622250d287fc3528a9f9de8b15499e0d54a00
Sebas Arango <sebastianarango201316@hotmail.com>
2024-03-14 21:26:09 +0100

    Basic test initiated for Engine

--- foundry-defi-stablecoin-f23/test/unit/DSCEngine.t.sol
+++ foundry-defi-stablecoin-f23/test/unit/DSCEngine.t.sol
@@ -14,20 +14,49 @@ contract DSCEngineTest is Test {
   DSCEngine public engine;
   HelperConfig public config;
   address public ethUsdPriceFeed;
+  address public btcUsdPriceFeed;
   address public weth;
 
   address public USER = makeAddr("user");
-  uint256 public constant MOCK_COLLATERAL = 10 ether;
+  uint256 public constant AMOUNT_COLLATERAL = 10 ether;
   uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
   function setUp() external {
     deployer = new DeployDSC();
     (dsc, engine, config) = deployer.run();
-    (ethUsdPriceFeed,, weth,) = config.activeNetworkConfig();
+    (ethUsdPriceFeed, btcUsdPriceFeed, weth,) = config.activeNetworkConfig();
 
     ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
   }
 
+  modifier depositedCollateral() {
+    vm.startPrank(USER);
+    ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+    engine.depositCollateral(weth, AMOUNT_COLLATERAL);
+    vm.stopPrank();
+    _;
+  }
+
+  address[] public tokenAddresses;
+  address[] public priceFeedAddresses;
+
+  function test_constructor_revertsWhenParametersLengthIsNotSame() public {
+    tokenAddresses.push(weth);
+    priceFeedAddresses.push(ethUsdPriceFeed);
+    priceFeedAddresses.push(btcUsdPriceFeed);
+
+
+    vm.expectRevert(DSCEngine.DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength.selector);
+    new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+  }
+
+  function test_getTokenAmountFromUsd_getsAmountCorrectly() public {
+    uint256 usdAmount = 100 ether;
+    uint256 expectedWeth = 0.05 ether;
+    uint256 actualWeth = engine.getTokenAmountFromUsd(weth, usdAmount);
+
+    assertEq(actualWeth, expectedWeth);
+  }
 
   function test_getUsdValue_canGetWETHValueCorrectly() public {
     uint256 ethAmount = 15e18;
@@ -39,10 +68,27 @@ contract DSCEngineTest is Test {
 
   function test_depositCollateral_revertsIfCollateralIsZero() public {
     vm.startPrank(USER);
-    ERC20Mock(weth).approve(address(engine), MOCK_COLLATERAL);
+    ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
 
     vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
     engine.depositCollateral(weth, 0);
     vm.stopPrank();
+  }
+
+  function test_depositCollateral_revertsWithUnapprovedCollateral() public {
+    ERC20Mock randomToken = new ERC20Mock();
+
+    vm.startPrank(USER);
+    vm.expectRevert(DSCEngine.DSCEngine__NotAllowedToken.selector);
+    engine.depositCollateral(address(randomToken), AMOUNT_COLLATERAL);
+  }
+
+  function test_depositCollateral_getCount() public depositedCollateral {
+    (uint256 totalDscMinted, uint256 collateralValueInUsd) = engine.getAccountInformation(USER);
+    uint256 expectedTotalDscMinted = 0;
+    uint256 expectedDepositAmount = engine.getTokenAmountFromUsd(weth, collateralValueInUsd);
+
+    assertEq(totalDscMinted, expectedTotalDscMinted);
+    assertEq(AMOUNT_COLLATERAL, expectedDepositAmount);
   }
 }
