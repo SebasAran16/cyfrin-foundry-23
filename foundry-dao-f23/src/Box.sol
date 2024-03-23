d0cd39c41000742ae5bd1c42a62124ce9402a7cb
Sebas Arango <sebastianarango201316@hotmail.com>
2024-03-23 21:50:02 +0100

    Added main DAO Smart Contracts

--- foundry-dao-f23/src/Box.sol
+++ foundry-dao-f23/src/Box.sol
@@ -1,8 +1,22 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
 
-contract Box {
-  constructor(){
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-  }
+contract Box is Ownable {
+    uint256 private s_number;
+
+    event NumberChanged(uint256 indexed newNumber);
+
+    constructor() Ownable(msg.sender) {}
+
+    function store(uint256 newNumber) external onlyOwner {
+        s_number = newNumber;
+
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() public view returns (uint256) {
+        return s_number;
+    }
 }
